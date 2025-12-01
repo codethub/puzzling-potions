060ce7937fcbfe4ff023cd9eb0ca06a3356da006
codethub <111586060+codethub@users.noreply.github.com>
2025-12-01 14:33:24 +0000

    change package and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
 // vite.config.ts
-
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react" // New Import!
 
 export default defineConfig({
+    plugins: [react()], // New Plugin!
     server: {
         port: 5000,
         open: true,
     },
-    base: "game",
-    
-    // [EDIT] Configuration to allow JSX inside .ts files
-    esbuild: {
-        // Force esbuild to process TypeScript (.ts) files as if they were TSX (.tsx)
-        loader: 'tsx',
-        include: [
-            // Ensure both .ts and .tsx files are processed for JSX
-            'src/**/*.ts', 
-            'src/**/*.tsx', 
-        ],
-        exclude: []
-    }
+    base: "pixi-game-match3",
 })
