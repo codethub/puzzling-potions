aca5c781f06ca93699da65f16a6d69f554248f94
codethub <111586060+codethub@users.noreply.github.com>
2025-12-01 14:47:31 +0000

    update index.tsx and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -105,5 +105,6 @@ export function App() {
             ref={appContainerRef} 
             style={{ width: '100%', height: '100%', overflow: 'hidden' }} 
         />
+        
     );
 }
--- src/web3/index.tsx
+++ src/web3/index.tsx
@@ -1,10 +1,9 @@
-'use client'
 
 import { wagmiAdapter } from './appkit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createAppKit } from '@reown/appkit/react'
 import { mainnet, arbitrum } from '@reown/appkit/networks'
-import React, { type ReactNode } from 'react'
+import { type ReactNode } from 'react'
 import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
 
 // Set up queryClient
